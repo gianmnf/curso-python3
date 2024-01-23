7c88373d3e87fbdf502c17cc6e78523e5885f4e0
Gian Michel <gianmnf@gmail.com>
2024-01-23 15:14:36 -0300

    Aula 7

---
+++ aula7.py
@@ -0,0 +1,19 @@
+# Variáveis
+
+"""
+PEP8: inicie variáveis com letras minúsculas, pode usar números e underline.
+O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome variável.
+Uso: nome_variavel = expressão
+"""
+
+nome_completo = 'Fulano de Tal Silva'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Gian'
+idade = 25
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
