bd747fdbe9bfe127badba90cc4afa83a4d1d22d0
Gian Michel <gianmnf@gmail.com>
2024-01-23 13:03:45 -0300

    Aula 3

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de Programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas Simples
+print('Gian Michel')
+print(1,'Gian "Michel"')
+
+# Aspas Duplas
+print("Gian Michel")
+print(2,"Gian 'Michel'")
+
+# Escape
+print("Gian \"Michel\"")
+
+# r
+print(r"Gian \"Michel\"")
