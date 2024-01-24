b9782e110612268d5f6b231fa1ff7454f1f2b7ba
Gian Michel <gianmnf@gmail.com>
2024-01-24 13:01:13 -0300

    Aula 13

---
+++ aula13.py
@@ -0,0 +1,9 @@
+# f-strings
+
+nome = 'Gian'
+altura = 1.86
+peso = 100
+imc = peso / altura ** 2
+resultado = f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu imc é {imc:.2f}'
+
+print(resultado)
