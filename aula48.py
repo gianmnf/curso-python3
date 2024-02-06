57452989011a77efb0401f60b48f23df920aee0f
Gian Michel <gianmnf@gmail.com>
2024-02-06 15:27:46 -0300

    Aula 48

---
+++ aula48.py
@@ -0,0 +1,17 @@
+""" 
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#         01234
+#        -54321
+string = 'ABCDE'
+
+#        0     1    2               3   4
+#       -5     -4  -3              -2   -1
+lista = [123, True, 'Gian Michel', 1.2, []]
+lista[-3] = 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
