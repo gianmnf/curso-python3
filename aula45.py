e3960868691b23c930f5b32ce112e0f22e4c0d7b
Gian Michel <gianmnf@gmail.com>
2024-02-06 12:21:53 -0300

    Aula 45

---
+++ aula45.py
@@ -0,0 +1,17 @@
+"""
+Iterável -> str, range, etc (__iter__)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+
+# for letra in texto
+texto = 'Gian' # iterável
+iterador = iter(texto) # iterator
+
+while True:
+    try:
+        letra = next(iterador)
+        print(letra)
+    except StopIteration:
+        break
